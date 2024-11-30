ad4b1b20ed01328de730403ff2e85d6cd1aa86ba
Kamil-Kowalczyk <kamil2005aaa@gmail.com>
2024-11-30 12:36:04 +0100

    cleared main component and added boostrap to the project

---
+++ frontend/src/app/hobby-suggestions/hobby-suggestions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hobby-suggestions',
+  standalone: true,
+  imports: [],
+  templateUrl: './hobby-suggestions.component.html',
+  styleUrl: './hobby-suggestions.component.scss'
+})
+export class HobbySuggestionsComponent {
+
+}
