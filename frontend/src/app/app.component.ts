a8eeb44fa8677134515c42bb9a6af4ea39ccd703
adamkupka05s@gmail.com <adamkupka05s@gmail.com>
2024-11-30 16:53:51 +0100

    formularz in progress

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HobbyService } from './hobby.service';
+import { HobbyFormComponent } from './hobby-form/hobby-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, HobbyFormComponent],
   providers: [HobbyService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- frontend/src/app/hobby-form/hobby-form.component.ts
+++ frontend/src/app/hobby-form/hobby-form.component.ts
@@ -1,12 +1,79 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HobbyForm } from '../hobby-form.model';
 
 @Component({
   selector: 'app-hobby-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './hobby-form.component.html',
   styleUrl: './hobby-form.component.scss'
 })
 export class HobbyFormComponent {
+
+  hobby: HobbyForm = new HobbyForm();
+  timeValue: number = 0;
+  step: number = 0;
+
+  constructor() { }
+
+  proceed(): void {
+    console.log('dupa');
+  }
+
+  getTimeValue(): void {
+    
+  }
+
+  stepAhead(): void {
+    this.step++;
+  }
+
+  riskyActivities(): void {
+    this.hobby.riskyActivities = true;
+    this.stepAhead();
+  }
+
+  isIndividualist(): void {
+    this.hobby.isIndividualist = true;
+    this.stepAhead();
+  }
+
+  isSpontanic(): void {
+    this.hobby.isSpontanic = true;
+    this.stepAhead();
+  }
+
+  freshAirActivities(): void {
+    this.hobby.freshAirActivities = true;
+    this.stepAhead();
+  }
+
+  isCreative(): void {
+    this.hobby.isCreative = true;
+    this.stepAhead();
+  }
+
+  manualActivities(): void {
+    this.hobby.manualActivities = true;
+    this.stepAhead();
+  }
+
+  likesScience(): void {
+    this.hobby.likesScience = true;
+    this.stepAhead();
+  }
+
+  likesHistory(): void {
+    this.hobby.likesHistory = true;
+    this.stepAhead();
+  }
+
+  likesNature(): void {
+    this.hobby.likesNature = true;
+    this.stepAhead();
+  }
+
 
 }
